1d0583e183e0e4d2d65802aeeace669d044b45ec
kaik-studies <164260383+kaik-studies@users.noreply.github.com>
2025-02-25 15:01:58 -0300

    Create Atv_002.java

---
+++ Atv_002.java
@@ -0,0 +1,16 @@
+package OLAVO;
+
+public class Atv_002 {
+
+}
+
+class Mamifero extends Animal {
+	String tipoPelo;
+	public void emitirSom() {
+		System.out.println("Som de mamifero");
+	}
+	public void movimentar() {
+		// TODO Auto-generated method stub
+		
+	}
+}
